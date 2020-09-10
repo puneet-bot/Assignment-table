c8bdf70b3f52f1220191b4db1d7e6e9b1e2fc8af
Praveen Kumar <pkspyder007@gmail.com>
2020-09-10 18:10:14 +0530

    Added PORT

--- app.js
+++ app.js
@@ -166,7 +166,7 @@ app.delete("/assignment/:id/inter",function(req,res){
     });
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 
 
 app.listen(port,function(){
